7828070fdeb4d3080179d79412606a75db62e9c2
naninunenoy <naninuneno0411@gmail.com>
2023-11-28 21:48:15 +0900

    Async EntryPoint

--- Assets/AntiMonoBehaviour/Scripts/ApplicationEntryPoint.cs
+++ Assets/AntiMonoBehaviour/Scripts/ApplicationEntryPoint.cs
@@ -1,4 +1,7 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace AntiMonoBehaviour
 {
@@ -7,7 +10,14 @@ namespace AntiMonoBehaviour
         [RuntimeInitializeOnLoadMethod]
         static void Entry()
         {
+            if (SceneManager.GetActiveScene().name != "Entry") return;
+            EntryAsync(Application.exitCancellationToken).Forget();
+        }
+
+        static UniTaskVoid EntryAsync(CancellationToken cancel)
+        {
             Debug.Log("Entry");
+            return default;
         }
     }
 }
